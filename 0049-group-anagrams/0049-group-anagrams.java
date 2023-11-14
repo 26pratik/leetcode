0d4c96e129eb089f144bcc90f1a9efe173ec3135
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-14 13:15:47 -0500

    Time: 5 ms (98.85%), Space: 46.7 MB (61.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,34 @@
-//Time Complexity : O(nk log(k))
+//Time Complexity : O(nk)
 class Solution {
+    
+    int[] primeArr;
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        if(strs == null || strs.length == 0) return new ArrayList<>();
+        primeArr = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
         
-        Map<String, List<String>> map = new HashMap<>();
+        Map<Double, List<String>> map = new HashMap<>();
         
-        for(String s : strs) { //log(n)
+        for(String s : strs) { //O(n)
+
+            double key = getHash(s);
             
-            char[] c = s.toCharArray();
-            Arrays.sort(c); //(k log(k))
-            String sorted = String.valueOf(c);
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList<>());
             
-            if(!map.containsKey(sorted))
-                map.put(sorted, new ArrayList<>());
-            
-            map.get(sorted).add(s);
+            map.get(key).add(s);
         }
         
         return new ArrayList<>(map.values());
+    }
+    
+    private double getHash(String s) {
+        
+        double hash = 1d;
+        
+        //O(k)
+        for(char ch : s.toCharArray())
+            hash *= primeArr[ch - 'a'];
+        
+        return hash;
     }
 }
