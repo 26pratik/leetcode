28624157670091c3d7a366e2bd5596f76127e05b
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-18 14:57:22 -0500

    Time: 16 ms (62.36%), Space: 44.3 MB (12.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,6 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
+
+            // return helper(coins, amount, 0, 0);
         int m = coins.length;
         int n = amount;
         
@@ -25,4 +26,20 @@ class Solution {
         
         return dp[m][n];
     }
+    
+//     private int helper(int[] coins, int amount, int index, int coinsUsed) {
+        
+//         // base
+//         if(index == coins.length || amount < 0) return -1;        
+//         if(amount == 0) return coinsUsed;
+        
+//         // logic        
+//         int case0 = helper(coins, amount, index+1, coinsUsed);
+//         int case1 = helper(coins, amount-coins[index], index, coinsUsed+1);
+        
+//         if(case0 == -1) return case1;
+//         if(case1 == -1) return case0;
+        
+//         return Math.min(case0, case1);
+//     }
 }
