1b4713d5123a5e749ff3024e55ecaa386a10a153
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-18 17:02:04 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (5.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,31 @@
 class Solution {
     public int rob(int[] nums) {
         
-        int n = nums.length;
+        // return helper(nums, 0, 0);
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
         
-        if(nums == null || n == 0) return 0;        
-        if(n == 1) return nums[0];
+        int[] dp = new int[nums.length];
         
-        int prev = nums[0];
-        int current = Math.max(nums[0], nums[1]);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
         
-        for(int i=2; i<n; i++) {
-            int temp = current;
-            current = Math.max(current, nums[i]+prev);
-            prev = temp;
+        for(int i=2; i<nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
         }
         
-        return current;
+        return dp[nums.length-1];
     }
+    
+//     private int helper(int[] nums, int index, int amount) {
+        
+//         //base
+//         if(index >= nums.length) return amount;
+        
+//         //logic
+//         int case0 = helper(nums, index+1, amount);
+//         int case1 = helper(nums, index+2, amount+nums[index]);
+        
+//         return Math.max(case0, case1);
+//     }
 }
