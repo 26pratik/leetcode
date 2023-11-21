b0ebaf2959ce2e002a3614597631131d015c49c3
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-20 19:26:38 -0500

    Time: 1 ms (85.71%), Space: 42.5 MB (52.76%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,19 +1,36 @@
 class Solution {
     public int minCost(int[][] costs) {
-        
+  
+        if(costs == null || costs.length == 0) return 0;
         int m = costs.length;
-        int n = costs[0].length;
-        int[][] dp = new int[m][n];
         
-        dp[0][0] = costs[0][0];
-        dp[0][1] = costs[0][1];
-        dp[0][2] = costs[0][2];
+        int[] dp = new int[3];
+        
+        dp[0] = costs[0][0];
+        dp[1] = costs[0][1];
+        dp[2] = costs[0][2];
         
         for(int i=1; i<m; i++) {
-            dp[i][0] = Math.min(dp[i-1][1]+costs[i][0], dp[i-1][2]+costs[i][0]);
-            dp[i][1] = Math.min(dp[i-1][0]+costs[i][1], dp[i-1][2]+costs[i][1]);
-            dp[i][2] = Math.min(dp[i-1][0]+costs[i][2], dp[i-1][1]+costs[i][2]);
-        }        
+            int tempR = dp[0];
+            int tempB = dp[1];
+            dp[0] = costs[i][0] + Math.min(tempB, dp[2]);
+            dp[1] = costs[i][1] + Math.min(tempR, dp[2]);
+            dp[2] = costs[i][2] + Math.min(tempR, tempB);
+        }  
+        
+        return Math.min(dp[0], Math.min(dp[1], dp[2]));  
+//         int n = costs[0].length;        
+//         int[][] dp = new int[m][n];
+        
+//         dp[0][0] = costs[0][0];
+//         dp[0][1] = costs[0][1];
+//         dp[0][2] = costs[0][2];
+        
+//         for(int i=1; i<m; i++) {
+//             dp[i][0] = Math.min(dp[i-1][1]+costs[i][0], dp[i-1][2]+costs[i][0]);
+//             dp[i][1] = Math.min(dp[i-1][0]+costs[i][1], dp[i-1][2]+costs[i][1]);
+//             dp[i][2] = Math.min(dp[i-1][0]+costs[i][2], dp[i-1][1]+costs[i][2]);
+//         }        
         
         // for(int i=1; i<m; i++) {
         //     for(int j=0; j<n; j++) {
@@ -26,7 +43,7 @@ class Solution {
         //     }
         // }
         
-        return Math.min(dp[m-1][0], Math.min(dp[m-1][1], dp[m-1][2]));
+        // return Math.min(dp[m-1][0], Math.min(dp[m-1][1], dp[m-1][2]));
     }
 }
 // class Solution {
