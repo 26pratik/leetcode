7ff80841ac075f3448c847254fe9ff040a9b73d1
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-20 19:16:08 -0500

    Time: 1 ms (85.71%), Space: 43 MB (23.85%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -10,15 +10,21 @@ class Solution {
         dp[0][2] = costs[0][2];
         
         for(int i=1; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(j == 0)
-                    dp[i][j] = Math.min(dp[i-1][1]+costs[i][j], dp[i-1][2]+costs[i][j]);
-                if(j == 1)
-                    dp[i][j] = Math.min(dp[i-1][0]+costs[i][j], dp[i-1][2]+costs[i][j]);
-                if(j == 2) 
-                    dp[i][j] = Math.min(dp[i-1][0]+costs[i][j], dp[i-1][1]+costs[i][j]);
-            }
-        }
+            dp[i][0] = Math.min(dp[i-1][1]+costs[i][0], dp[i-1][2]+costs[i][0]);
+            dp[i][1] = Math.min(dp[i-1][0]+costs[i][1], dp[i-1][2]+costs[i][1]);
+            dp[i][2] = Math.min(dp[i-1][0]+costs[i][2], dp[i-1][1]+costs[i][2]);
+        }        
+        
+        // for(int i=1; i<m; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         if(j == 0)
+        //             dp[i][j] = Math.min(dp[i-1][1]+costs[i][j], dp[i-1][2]+costs[i][j]);
+        //         if(j == 1)
+        //             dp[i][j] = Math.min(dp[i-1][0]+costs[i][j], dp[i-1][2]+costs[i][j]);
+        //         if(j == 2) 
+        //             dp[i][j] = Math.min(dp[i-1][0]+costs[i][j], dp[i-1][1]+costs[i][j]);
+        //     }
+        // }
         
         return Math.min(dp[m-1][0], Math.min(dp[m-1][1], dp[m-1][2]));
     }
