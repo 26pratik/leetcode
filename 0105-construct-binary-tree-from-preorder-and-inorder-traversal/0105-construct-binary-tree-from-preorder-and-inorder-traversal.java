b83a8de3d5af567e6e2b1dfee4925d0db7e8ef11
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-30 18:49:39 -0500

    Time: 7 ms (12.94%), Space: 44.3 MB (11.76%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,44 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    
-    Map<Integer, Integer> map;
-    int index;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
-        map = new HashMap<>();
-        index = 0;
-        for(int i=0; i<inorder.length; i++)
-            map.put(inorder[i], i);
-        
-        return helper(preorder, 0, inorder.length-1);
-    }
-    
-    private TreeNode helper(int[] preorder, int start, int end) {
 
-        if(start > end) return null;  
+        if(preorder.length == 0) return null;
         
-        int rootValue = preorder[index];
-        index++; 
-        int rIdx = map.get(rootValue);
-        
+        int rootValue = preorder[0];
+        int rIdx = -1;
+        for(int i=0; i<inorder.length; i++)
+            if(inorder[i] == rootValue)
+                rIdx = i;
+
+        int[] inLeft = Arrays.copyOfRange(inorder, 0, rIdx);
+        int[] inRight = Arrays.copyOfRange(inorder, rIdx+1, inorder.length);
+        int[] preLeft = Arrays.copyOfRange(preorder, 1, 1+inLeft.length);
+        int[] preRight = Arrays.copyOfRange(preorder, 1+inLeft.length, preorder.length);
+
         TreeNode root = new TreeNode(rootValue);
-        root.left = helper(preorder, start, rIdx-1);
-        root.right = helper(preorder, rIdx+1, end);
-        
-        return root;        
+        root.left = buildTree(preLeft, inLeft);
+        root.right = buildTree(preRight, inRight);
+
+        return root;       
     }
 }
