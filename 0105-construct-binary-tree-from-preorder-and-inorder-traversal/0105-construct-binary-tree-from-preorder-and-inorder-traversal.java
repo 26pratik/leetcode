9ab83831d78105ab0d2219b0302e30f6e7dfc41d
26pratik <37126666+26pratik@users.noreply.github.com>
2023-11-30 14:02:57 -0500

    Time: 1 ms (93.76%), Space: 43.2 MB (52.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,31 +16,28 @@
 class Solution {
     
     Map<Integer, Integer> map;
+    int index;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
         map = new HashMap<>();
-        
+        index = 0;
         for(int i=0; i<inorder.length; i++)
             map.put(inorder[i], i);
         
-        return helper(preorder, inorder);
+        return helper(preorder, 0, inorder.length-1);
     }
     
-    private TreeNode helper(int[] preorder, int[] inorder) {
+    private TreeNode helper(int[] preorder, int start, int end) {
 
-        if(preorder.length == 0) return null;  
+        if(start > end) return null;  
         
-        int rootValue = preorder[0];
+        int rootValue = preorder[index];
+        index++; 
         int rIdx = map.get(rootValue);
         
-        int[] inLeft = Arrays.copyOfRange(inorder, 0, rIdx);
-        int[] inRight = Arrays.copyOfRange(inorder, rIdx+1, inorder.length);
-        int[] preLeft = Arrays.copyOfRange(preorder, 1, 1+inLeft.length);
-        int[] preRight = Arrays.copyOfRange(preorder, 1+inLeft.length, preorder.length);
-        
         TreeNode root = new TreeNode(rootValue);
-        root.left = buildTree(preLeft, inLeft);
-        root.right = buildTree(preRight, inRight);
+        root.left = helper(preorder, start, rIdx-1);
+        root.right = helper(preorder, rIdx+1, end);
         
         return root;        
     }
