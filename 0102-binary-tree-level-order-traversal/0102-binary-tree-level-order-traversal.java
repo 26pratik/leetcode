173efb82c3d533a53e5f0da9506cdb7c533188d7
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-05 14:28:10 -0500

    Time: 0 ms (100.00%), Space: 44.2 MB (30.23%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,35 +17,24 @@
 //SC: O(n/2) -> O(n)
 //BFS
 class Solution {
+    
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-        if(root == null) return new ArrayList<>();
-        
-        List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        q.add(root);
-        
-        while(!q.isEmpty()) {
-            
-            int size = q.size();
-            List<Integer> list = new ArrayList<>();
-            
-            for(int i=0; i<size; i++) {
-                
-                TreeNode node = q.poll();
-                list.add(node.val);
-                
-                if(node.left != null)
-                    q.add(node.left);
-                
-                if(node.right != null)
-                    q.add(node.right);
-            }
-            
-            result.add(list);
-        }
+        helper(root, 0);
         
         return result;
+    }
+    
+    private void helper(TreeNode root, int depth) {
+        
+        if(root == null) return;
+        
+        if(result.size() == depth) 
+            result.add(new ArrayList<>());
+        result.get(depth).add(root.val);
+        
+        helper(root.left, depth+1);
+        helper(root.right, depth+1);
     }
 }
