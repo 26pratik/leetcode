04cc8e50394baef5dcdd0d51ee5eac87e9d4695f
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-08 20:31:48 -0500

    Time: 11 ms (87.33%), Space: 45.2 MB (84.59%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        
+        int[][] dirs = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
+        Queue<int[]> q = new LinkedList<>();
+        int m = mat.length;
+        int n = mat[0].length;
+        int dist = 1;
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(mat[i][j] == 0)
+                    q.add(new int[]{i,j});
+                else if(mat[i][j] == 1)
+                    mat[i][j] = -1;
+            }
+        }
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0; i<size; i++) {
+                int[] curr = q.poll();
+                
+                for(int[] dir: dirs) {
+                    int row = curr[0] + dir[0];
+                    int col = curr[1] + dir[1];
+                    
+                    if(row>=0 && col>=0 && row<m && col<n && mat[row][col] == -1) {
+                        mat[row][col] = dist;
+                        q.add(new int[]{row, col});
+                    }
+                }
+            }
+            dist++;
+        }
+        
+        return mat;
+    }
+}
