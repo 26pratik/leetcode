c5b9de76adb35fcf418dc73e8ebf8d4d379b311b
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-16 17:21:40 -0500

    Time: 0 ms (100.00%), Space: 40.5 MB (59.36%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,39 +1,34 @@
+//TC: O(k*l) -> O(L) where L is length of resultant string
+//SC: O(L)
 class Solution {
+    
+    int i=0;
     public String decodeString(String s) {
-        
-        Stack<StringBuilder> strSt = new Stack<>();
-        Stack<Integer> numSt = new Stack<>();
         
         int currNum = 0;
         StringBuilder currStr = new StringBuilder();
         
-        for(int i=0; i<s.length(); i++) {
+        while(i < s.length()) {
             char c = s.charAt(i);
-            
+            i++;
             if(Character.isDigit(c)) {
                 currNum = currNum * 10 + c - '0';
             }
             else if(c == '[') {
-                strSt.push(currStr);
-                numSt.push(currNum);
+                String result = decodeString(s);
+                for(int i=0; i<currNum; i++) {
+                    currStr.append(result);
+                }
                 currNum = 0;
-                currStr = new StringBuilder();
             }
             else if(c == ']') {
-                StringBuilder temp = new StringBuilder();
-                int num = numSt.pop();
-                for(int j=0; j<num; j++) {
-                    temp.append(currStr);
-                }
-                
-                currStr = strSt.pop();
-                currStr.append(temp);
+                return currStr.toString();
             }
             else {
                 currStr.append(c);
             }
         }
             
-            return currStr.toString();
+        return currStr.toString();
     }
 }
