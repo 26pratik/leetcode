59403012bf9e29cac04afc89fb81a7ba1f5aaf20
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-16 20:47:07 -0500

    Time: 2 ms (75.79%), Space: 44 MB (36.90%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -9,25 +9,23 @@ class Solution {
         return result;
     }
     
-    private void helper(int[] candidates, int target, int i, List<Integer> path) {
+    private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
         
         //base
-        if(target < 0 || i == candidates.length) return;
+        if(target < 0 || pivot == candidates.length) return;
         
         if(target == 0) {
             result.add(new ArrayList<>(path));
-            return;
         }
         
         //logic
-        
-        
-        helper(candidates, target, i+1, path);        
-        //action
-        path.add(candidates[i]);
-        //recurse
-        helper(candidates, target-candidates[i], i, path);
-        //backtrack
-        path.remove(path.size()-1);
+        for(int i=pivot; i<candidates.length; i++) {
+            //action
+            path.add(candidates[i]);
+            //recurse
+            helper(candidates, target-candidates[i], i, path);
+            //backtrack
+            path.remove(path.size()-1);    
+        }
     }
 }
