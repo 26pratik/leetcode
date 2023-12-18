46a51fa93f2e887d5925aff0dc683ff0b342e412
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-18 00:01:57 -0500

    Time: 1 ms (71.14%), Space: 42.4 MB (9.65%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,3 +1,5 @@
+//TC: O (n * 2*n)
+//SC: O()
 class Solution {
     
     List<List<Integer>> result;
@@ -8,16 +10,21 @@ class Solution {
         return result;
     }
     
+    //O(2^n )
     private void helper(int[] nums, int pivot, List<Integer> path) {
         
         //base
-        result.add(new ArrayList<>(path));
-        if(pivot == nums.length) return;
-
-        for(int i=pivot; i<nums.length; i++) {
-            path.add(nums[i]);
-            helper(nums, i+1, path);
-            path.remove(path.size()-1);
+        //O(n)
+        
+        if(pivot == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
         }
+        
+        helper(nums, pivot+1, path);
+        
+        path.add(nums[pivot]);
+        helper(nums, pivot+1, path);
+        path.remove(path.size()-1);
     }
 }
