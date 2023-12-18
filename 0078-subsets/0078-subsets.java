a690438fc2403526604a7190435f1d2c184e3aa2
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-17 23:16:56 -0500

    Time: 1 ms (71.14%), Space: 42.3 MB (16.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        this.result = new ArrayList<>();
+        helper(nums, 0, new ArrayList<>());
+        return result;
+    }
+    
+    private void helper(int[] nums, int pivot, List<Integer> path) {
+        
+        //base
+        if(pivot > nums.length) return;
+        
+        result.add(new ArrayList<>(path));
+        
+        for(int i=pivot; i<nums.length; i++) {
+            path.add(nums[i]);
+            helper(nums, i+1, path);
+            path.remove(path.size()-1);
+        }
+    }
+}
