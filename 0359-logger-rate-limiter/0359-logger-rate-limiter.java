3e9323a11ff938ee64feffde2336860cde5e3679
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-18 20:16:57 -0500

    Time: 29 ms (74.67%), Space: 54.9 MB (19.16%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.java
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.java
@@ -1,19 +1,17 @@
 class Logger {
 
     Map<String, Integer> map;
+    
     public Logger() {
         map = new HashMap<>();
     }
     
     public boolean shouldPrintMessage(int timestamp, String message) {
         
-        if(map.containsKey(message))
-            if(map.get(message) + 10 > timestamp)
-                return false;
-            else
-                map.put(message, timestamp);
-        else
-            map.put(message, timestamp);
+        if(map.containsKey(message) && map.get(message) + 10 > timestamp)
+            return false;
+
+        map.put(message, timestamp);
         
         return true;
     }
