9c46ea03f804389c9c24a958b024f00b4d124896
26pratik <37126666+26pratik@users.noreply.github.com>
2023-12-28 21:36:50 -0500

    Time: 28 ms (20.09%), Space: 45.4 MB (6.65%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -1,7 +1,7 @@
 class Solution {
     
     TrieNode root;
-    String result;
+    
     class TrieNode {
         boolean isEnd;
         TrieNode[] children;
@@ -9,20 +9,6 @@ class Solution {
         public TrieNode() {
             this.children = new TrieNode[26];
         }
-    }
-    
-    
-    public String longestWord(String[] words) {
-        
-        this.root = new TrieNode();
-        this.result = "";
-        
-        for(String word: words)
-            insert(word);
-        
-        dfs(root, new StringBuilder());
-        
-        return result;
     }
     
     private void insert(String word) {
@@ -35,24 +21,32 @@ class Solution {
         curr.isEnd = true;
     }
     
-    private void dfs(TrieNode root, StringBuilder path) {
+    public String longestWord(String[] words) {
         
-        //base
-        if(result.length() < path.length())
-            result = path.toString();
+        this.root = new TrieNode();
         
-        //logic
-        TrieNode curr = root;
-        for(int i=0; i<26; i++) {
-            if(curr.children[i] != null && curr.children[i].isEnd) {
-                int len = path.length();
-                //action
-                path.append((char)(i+'a'));
-                //recurse
-                dfs(curr.children[i], path);
-                //backtrack
-                path.setLength(len);
+        for(String word: words)
+            insert(word);
+        
+        String path = "";
+        Queue<TrieNode> q = new LinkedList<>();
+        Queue<String> strQ = new LinkedList<>();
+        
+        q.add(root);
+        strQ.add("");
+        
+        while(!q.isEmpty()) {
+            TrieNode curr = q.poll();
+            path = strQ.poll();
+            
+            for(int i=25; i>=0; i--) {
+                if(curr.children[i] != null && curr.children[i].isEnd) {
+                    q.add(curr.children[i]);
+                    strQ.add(path + (char)(i + 'a'));
+                }
             }
         }
+        
+        return path;
     }
 }
