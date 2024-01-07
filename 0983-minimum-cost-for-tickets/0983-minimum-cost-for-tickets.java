04b546ea17258347516ffd0e081af3b73e899ad5
26pratik <37126666+26pratik@users.noreply.github.com>
2024-01-07 14:29:13 -0500

    Time: 1 ms (80.97%), Space: 41.2 MB (34.98%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,21 +1,23 @@
 class Solution {
     
-    int[] dp;
+    int dp[];
     public int mincostTickets(int[] days, int[] costs) {
 
-        int costIndex = 0;
         int max = days[days.length-1];
+        int dayIndex = 0;
         this.dp = new int[max + 1];
-
-        for (int j = 1; j <= max; j++) {
-            if (days[costIndex] > j) dp[j] = dp[j - 1];
+        
+        for(int i=1; i<=max; i++) {
+            if(days[dayIndex] > i) 
+                dp[i] = dp[i-1];
             else {
-                dp[j] = Math.min(dp[j - 1] + costs[0],
-                        Math.min(dp[Math.max(0, j - 7)] + costs[1], dp[Math.max(0, j - 30)] + costs[2]));
-                costIndex++;
+                dp[i] = Math.min(dp[i-1] + costs[0], 
+                                 Math.min(dp[Math.max(0, i-7)] + costs[1], 
+                                                             dp[Math.max(0, i-30)] + costs[2]));
+                dayIndex++;
             }
         }
-
-        return dp[max];        
+        
+        return dp[max];
     }
 }
