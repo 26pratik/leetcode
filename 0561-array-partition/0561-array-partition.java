49e228d7d0a0e77c0423c8f8100ebd5ac5485073
26pratik <37126666+26pratik@users.noreply.github.com>
2024-01-11 19:20:14 -0500

    Time: 5 ms (98.59%), Space: 46.6 MB (19.94%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,34 +1,33 @@
 class Solution {
+    
+    final static int k = 10000;
+    
     public int arrayPairSum(int[] nums) {
         
-        int min = Integer.MAX_VALUE;
+        int[] arr = new int[2*k+1];
         int max = Integer.MIN_VALUE;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
         int result = 0;
         
         for(int n: nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
             max = Math.max(max, n);
             min = Math.min(min, n);
+            arr[k+n]++;
         }
-        
+
         boolean flag = false;
-        
         for(int i=min; i<=max; i++) {
-            if(map.containsKey(i)) {
-                if(flag)
-                    map.put(i, map.get(i)-1);
-                
-                int count = map.get(i);
-                
-                result += count/2*i;
-                if(count%2 == 0) {
+            if(arr[k+i] > 0) {
+                if(flag) arr[k+i]--;
+                int cnt = arr[k+i];
+                result += cnt/2*(i);
+                if(cnt%2 == 0) {
                     flag = false;
                 }
                 else {
                     flag = true;
-                    result += i;
-                }
+                    result += (i);
+                }                
             }
         }
         
