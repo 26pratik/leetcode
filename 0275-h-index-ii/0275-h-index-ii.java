8e87d898bd21def721f13124a1b3dc3fba7252b0
26pratik <37126666+26pratik@users.noreply.github.com>
2024-01-13 22:53:18 -0500

    Time: 0 ms (100.00%), Space: 47.7 MB (82.63%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -2,13 +2,20 @@ class Solution {
     public int hIndex(int[] citations) {
         
         int n = citations.length;
+        int low = 0, high = n-1;
         
-        for(int i=0; i<n; i++) {
-            int diff = n-i;
-            if(citations[i] >= diff)
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            int diff = n - mid;
+            if(citations[mid] == diff)
                 return diff;
+            else if(citations[mid] > diff)
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         
-        return 0;
+        return n - low;
+        
     }
 }
