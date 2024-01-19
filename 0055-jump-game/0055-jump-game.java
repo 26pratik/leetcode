f4759b4b816e10185f190d7ffae1c359babdbb8c
26pratik <37126666+26pratik@users.noreply.github.com>
2024-01-19 14:15:08 -0500

    Time: 167 ms (14.27%), Space: 45.4 MB (41.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,31 +5,26 @@ class Solution {
         
         if(nums.length <= 1) return true;
         this.arr = new int[nums.length];  //0:unexplored, 1:explored and true, -1: explored nad false 
-        return dfs(nums, 0);
+        int result = dfs(nums, 0);
+        if(result == 1) return true;
+        return false;
     }
     
-    private boolean dfs(int[] nums, int idx) {
+    private int dfs(int[] nums, int idx) {
         
         //base
-        if(idx >= nums.length-1) return true;
-        if(arr[idx] == 1) return true;
-        if(arr[idx] == -1) return false;
+        if(idx >= nums.length-1) return 1;
+        if(arr[idx] != 0) return arr[idx];
         
         //logic
         for(int i=1; i<=nums[idx]; i++) {
             int newIdx = idx+i;
             if(arr[newIdx] == 0) {
-                boolean temp = dfs(nums, newIdx);
-                if(temp) {
-                    arr[newIdx] = 1;
-                    return true;
-                }
-                else {
-                    arr[newIdx] = -1;
-                }
+                arr[newIdx] = dfs(nums, newIdx);
             }
+            if(arr[newIdx] == 1) return 1;
         }
         arr[idx] = -1;
-        return false;
+        return -1;
     }
 }
